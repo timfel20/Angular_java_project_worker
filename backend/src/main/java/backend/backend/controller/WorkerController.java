e87672eb7aca145c8923371fcbfb24c84c134d14
timfel20 <timmyfelicia@gmail.com>
2023-09-24 21:26:19 +0200

    create data functionality added

--- backend/src/main/java/backend/backend/controller/WorkerController.java
+++ backend/src/main/java/backend/backend/controller/WorkerController.java
@@ -3,12 +3,18 @@ package backend.backend.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import backend.backend.model.Worker;
 import backend.backend.repository.WorkerRepository;
+
+
+@CrossOrigin(origins = "http://localhost:4200") 
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -22,4 +28,9 @@ public class WorkerController {
 	public List<Worker> getAllWorkers(){
 		return workerRepository.findAll();
 	}	
+	
+	@PostMapping("/workers")
+	public Worker createWorkers(@RequestBody Worker worker) {
+		return workerRepository.save(worker);
+	}
 }
